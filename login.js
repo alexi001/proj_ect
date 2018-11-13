d8763c948cf3274dee7974155803fdcc36ee3835
alexi001 <harshshrm246@gmail.com>
2018-11-13 20:01:06 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,26 @@
+   	function checkCredentials(){
+		var x = document.getElementById("name").value;
+		var y = document.getElementById("pwd").value;
+		if(x == "shobhit" && y == "123"){
+			window.alert("login Successfull");
+		}
+		else{
+			window.alert("Login Failed: Username or id is wrong");
+		}
+	}
+
+	function changeUserInProperty(){
+		document.getElementById("usr").style.color = "black";
+	}
+
+	function changePassInProperty(){
+		document.getElementById("ps").style.color = "red";
+	}
+
+	function changeUserOutProperty(){
+		document.getElementById("usr").style.color = "blue";
+	}
+
+	function changePassOutProperty(){
+		document.getElementById("ps").style.color = "blue";
+	}
